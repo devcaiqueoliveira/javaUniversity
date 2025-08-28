2525f39adb3870c974d3b82aab2b605f3afd2ae8
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-08-28 15:56:30 -0300

    feat: adicionada funcionalidades da sprint 2

--- src/domain/Main.java
+++ src/domain/Main.java
@@ -9,8 +9,38 @@ public class Main {
         String name = sc.nextLine();
         System.out.println("Digite as notas das 3 matérias do aluno. Matemática, Física e Química:");
         int[] grades = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
-        System.out.println("Matemática: " + grades[0] + " Física: " + grades[1] + " Química: " + grades[2]);
+        String[] disciplinas = {"Matemática","Física","Química"};
 
+        for(int i=0;i<grades.length;i++){
+            System.out.print(disciplinas[i]+": " + grades[i]+", ");
+        }
+        sc.close();
+
+        float media=0;
+        for(int nota: grades){
+            media += nota;
+        }
+        media /= grades.length;
+        System.out.println("\n Média: " + media);
+
+        if(media>=60){
+            System.out.println("Aprovado");
+        }else{
+            System.out.println("Reprovado");
+        }
+
+        int max=grades[0], min =grades[0];
+        for(int nota: grades){
+            if(nota>max){
+                max = nota;
+            }
+            if(nota<min){
+                min=nota;
+            }
+        }
+
+        System.out.println("Alta: "+max);
+        System.out.println("Baixa: "+min);
         sc.close();
     }
 }
